de092a1a68f91317711f40b46e61f9edc64a54d4
KaanKara <41960384+karaKaan@users.noreply.github.com>
2022-02-12 14:03:18 +0100

    init react with ts, created header component

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './app/components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title='Hello World' color='red' />
+    </div>
+  );
+}
+
+export default App;
