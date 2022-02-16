79accd8c0a5f00d015a0534db9d6bc936330a953
KaanKara <41960384+karaKaan@users.noreply.github.com>
2022-02-16 09:50:57 +0100

    shrink the block from nav

--- next-ts-landingpage/app/components/module/Navbar/style.ts
+++ next-ts-landingpage/app/components/module/Navbar/style.ts
@@ -8,9 +8,10 @@ box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
 export const NavItemWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: baseline;
-  padding-block: 1.8em;
+  align-items: center;
+  padding-block: .75em;
 `;
+
 export const LinkWrapper = styled.div`
   display: flex;
   column-gap: 2em;
